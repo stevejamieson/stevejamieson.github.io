3c0184979222473d91e900de2a8d1189a9e502ea
Steve Jamieson <stevejamieson@gmail.com>
2024-12-10 17:02:46 -0400

    testing

--- assets/js/bluerain.js
+++ assets/js/bluerain.js
@@ -22,7 +22,7 @@ let rainFontNumber = 0;
 let showEmojis = true;
 let showTextShadow = false;
 
-const canvas = document.querySelector("canvas"),
+const canvas = document.querySelector("container"),
     ctx = canvas.getContext("2d");
 
 canvas.width = window.innerWidth;
